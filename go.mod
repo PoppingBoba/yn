3f130c2ab4758a03a6fddd112985e1f5f5899485
Nakada Tokumei <nakada_tokumei@protonmail.com>
2025-11-10 09:00:57 +0900

    [Develop] Working on json2ninja

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/PoppingBoba/yn
 go 1.25.4
 
 require github.com/PoppingBoba/ninjago v0.0.0-20251109113507-af6e7d1a338f
+
+require github.com/goccy/go-yaml v1.18.0 // indirect
--- src/main.go
+++ src/main.go
@@ -1,31 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/PoppingBoba/ninjago"
+	"github.com/PoppingBoba/yn/src/yn"
 )
 
-func test_ninjago() {
-	var cxx_rule = ninjago.MakeNinjaGoRule("cxx", "g++ -c $in -o $out $cflags $includes", "CXX $out")
-
-	fmt.Println("========== Check CXX Rule ==========")
-	fmt.Println(cxx_rule.String())
-}
-
 func main() {
-	fmt.Print("Hello yn\n")
-
-	var arg_parser = GetArgsParser()
-
-	arg_parser.AddArgument("-b", "./", "Set Build Path")
-	arg_parser.AddArgument("-f", "yn.yaml", "Set yn File")
-
-	arg_parser.Parse(os.Args)
-
-	fmt.Println("Check Argument Test [-b] : ", arg_parser.Get("-b").value)
-	fmt.Println("Check Argument Test [-f] : ", arg_parser.Get("-f").value)
-
-	test_ninjago()
+	yn.Main()
 }
